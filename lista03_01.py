401b8709bc0f049ba9515e06074158f4efea0090
braczkow <braczkow@gmail.com>
2014-04-24 19:50:05 +0200

    timeit

--- lista03_01.py
+++ lista03_01.py
@@ -1,16 +1,21 @@
+import timeit
+
+iterations = 10000
+
 def is_prime(x):
 	return not [y for y in range(2, x) if x % y == 0]
 	
 def prime_list_coh(n):
-	print [x for x in range(2,n) if is_prime(x)]
+	return [x for x in range(2,n) if is_prime(x)]
 	
 def prime_list_fil(n):
-	print filter(is_prime, range(2, n))
+	return filter(is_prime, range(2, n))
 
-prime_list_coh(20)
+print timeit.timeit('prime_list_coh(100)', 
+'from __main__ import prime_list_coh', number = iterations)
 
-prime_list_fil(20)
-
+print timeit.timeit('prime_list_fil(100)', 
+'from __main__ import prime_list_fil', number = iterations)
 
 
 
