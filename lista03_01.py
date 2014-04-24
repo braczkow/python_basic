4d19ded1f6cbed09df2491d6761577e97233b412
braczkow <braczkow@gmail.com>
2014-04-24 19:37:11 +0200

    prime_list_coh

--- lista03_01.py
+++ lista03_01.py
@@ -1,8 +1,11 @@
 def is_prime(x):
 	return not [y for y in range(2, x) if x % y == 0]
 	
-print is_prime(8)
+def prime_list_coh(n):
+	print [x for x in range(2,n) if is_prime(x)]
 
-print is_prime(7)
+prime_list_coh(20)
+
+
 
 
