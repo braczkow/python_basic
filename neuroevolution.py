c61762bc5c6e185794ab262a48c7eca4173c9b61
braczkow <braczkow@gmail.com>
2014-06-22 20:28:59 +0200

    Ids added to Neuron

--- neuroevolution.py
+++ neuroevolution.py
@@ -1,8 +1,14 @@
+import random
+
+random.seed(3)
+
 class Neuron:
-	def __init__(self, N):
+	def __init__(self, N, id):
 		self.weights = []
 		for i in range (0, N):
-			self.weights.append(1);
+			self.weights.append(random.uniform(-1, 1));
+		
+		self.weights[id] = 0
 		
 	def __str__(self):
 		return "(" + '; '.join([str(w) for w in self.weights]) + ")"
@@ -11,8 +17,8 @@ class NeuroSystem:
 	def __init__(self, N):
 		self.N = N;
 		self.neurons = []
-		for x in range(0, N):
-			self.neurons.append(Neuron(N));
+		for i in range(0, N):
+			self.neurons.append(Neuron(N, i));
 			
 	def __str__(self):
 		return "N: " + str(self.N) +'\n' + ', '.join([str(n) for n in self.neurons])
