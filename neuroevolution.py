344fdafb4628b157f391392b04e3025d3086a6e1
braczkow <braczkow@gmail.com>
2014-06-26 22:54:24 +0200

    Functional NeuroEvolution algorithm over a Hopfield network

--- neuroevolution.py
+++ neuroevolution.py
@@ -1,14 +1,23 @@
 import random
 import math
+import copy
 
-random.seed(3)
+#random.seed(3)
 
-MUTATION_RATE = 0.05
+GENERATIONS = 500
+POPULATION = 20
+
+NEURONS = 7
+
+MUTATION_RATE = 0.02
 MUTATION_MEAN = 0
-MUTATION_SIGMA = 1
+MUTATION_SIGMA = 0.5
+
+STABILITY_LEVEL = 0.01
 
 class Neuron:
 	def __init__(self, N, id):
+		self.N = N
 		self.weights = []
 		self.id = id
 		self.value = 0
@@ -22,8 +31,8 @@ class Neuron:
 	def __str__(self):
 		return "id: " + str(self.id) + " value: " + str(self.value) + "(" + '; '.join([str(w) for w in self.weights]) + ")"
 
-	def catchInputs(self, currentState):
-		self.catchedInput = sum(p*q for p,q in zip(self.weights, currentState))
+	def catchInputs(self, current_state):
+		self.catchedInput = sum(p*q for p,q in zip(self.weights, current_state))
 
 	def update(self):
 		self.value = 1/(1 + math.exp(-1 * self.beta * self.catchedInput))
@@ -33,7 +42,7 @@ class Neuron:
 		global MUTATION_MEAN
 		global MUTATION_SIGMA
 
-		for i in range (0, N):
+		for i in range (0, self.N):
 			rand = random.uniform(0, 1)
 			if rand < MUTATION_RATE:
 				change = random.normalvariate(MUTATION_MEAN, MUTATION_SIGMA)
@@ -54,17 +63,17 @@ class NeuroSystem:
 		return "N: " + str(self.N) +'\n' + ', '.join([str(n) for n in self.neurons])
 
 	def _advance(self):	
-		currentState = [n.value for n in self.neurons];
+		current_state = [n.value for n in self.neurons];
 			
 		for i in range(0, self.N):
-			self.neurons[i].catchInputs(currentState);
+			self.neurons[i].catchInputs(current_state);
 
 		for i in range(0, self.N):
 			self.neurons[i].update();
+
+
 		
-	def step(self, inputs):
-		print "inputs: " + ", ".join([str(i) for i in inputs])
-			
+	def step(self, inputs):		
 		if len(inputs) > self.N:
 			print "len(inputs) > self.N"
 			return
@@ -72,20 +81,37 @@ class NeuroSystem:
 		for i in range(0, len(inputs)):
 			self.neurons[i].value = inputs[i]
 
-		#currentState = [n.value for n in self.neurons];
-		#print currentState	
+		#current_state = [n.value for n in self.neurons];
+		#print current_state	
 
 		#TODO add stabilization
-		self._advance()
+		i = 0
+		not_stable = True
+		while i < 100 and not_stable:
+			current_state = [n.value for n in self.neurons];
 
-		#currentState = [n.value for n in self.neurons];
-		#print currentState	
+			self._advance()
+			
+			new_state = [n.value for n in self.neurons];
 
-		return [self.neurons[i] for i in range(self.N - self.n_to_return, self.N)]
+			diff = [(p-q)*(p-q) for p, q in zip(current_state, new_state)]
+			scalar_diff = sum(diff)
+
+			global STABILITY_LEVEL
+
+			if scalar_diff < STABILITY_LEVEL:
+				not_stable = False
+
+			i = i+1
+
+		#current_state = [n.value for n in self.neurons];
+		#print current_state	
+
+		return [self.neurons[i].value for i in range(self.N - self.n_to_return, self.N)]
 
 	def _cross(self, partner):
 		if self.N != partner.N:
-			print "We cannot DO IT!"
+			print "We cannot DO it!"
 			return
 
 		neurons = []
@@ -119,51 +145,81 @@ class GeneticAlgorithm:
 
 		self.leader = None
 
-	def _one_generation(self):
-		fitnesses = [p.fitness = fitness_function(p) for p in self.currentPopulation]
-		sorted(self.currentPopulation, key=lambda p: p.fitness)
+	def _process_generation(self):
+		for p in self.current_generation:
+			p.fitness = self.fitness_function(p)
+
+		sorted(self.current_generation, key=lambda p: p.fitness)
+		
+
+		leader_candidate = self.current_generation[0]
+
+		if leader_candidate.fitness > self.leader.fitness:
+			print "Changed leader from ", self.leader.fitness, " to ", leader_candidate.fitness
+			self.leader = copy.deepcopy(leader_candidate)
+
+
+		parents = self.current_generation[0 : self.n_population/2]
+		#print "Parents lenght: ", len(parents)
+		new_generation = []
+
+		while len(parents) > 0:
+			parent_a = parents.pop(random.randrange(0, len(parents)))
+			parent_b = parents.pop(random.randrange(0, len(parents)))
+
+			new_generation.append(parent_a.evolve(parent_b))
+			new_generation.append(parent_a.evolve(parent_b))
+			new_generation.append(parent_a)
+			new_generation.append(parent_b)
+
+
+		self.current_generation = new_generation
+
+
 
 	def run(self, n_generations, n_population):
 		self.n_generations = n_generations
 		self.n_population = n_population
 
-		self.currentPopulation = [class_type() for i in range(n_population)]
+		self.current_generation = [self.class_type() for i in range(n_population)]
+		self.leader = self.current_generation[0]
+		self.leader.fitness = 0
 
 		for i in range(0, self.n_generations):
-			self._one_generation()
+			self._process_generation()
 
 		return self.leader
 
 
-def NeuroSystemDef():
-	return NeuroSystem(5, 1)
+def neuro_system_impl():
+	global NEURONS
+	return NeuroSystem(NEURONS, 1)
 
 def xor_fitness(neuro_system):
-	return 1
+	inputs =[[0, 0], [0, 1], [1, 0], [1, 1]]
+	expected = [0, 1, 1, 0]
+	actual = [neuro_system.step(i) for i in inputs]
 
+	diff = [(p - q[0])*(p - q[0]) for p, q in zip(expected, actual)]
 
-ga = GeneticAlgorithm(NeuroSystemDef, xor_fitness)
+	scalar_diff = sum(diff)
 
+	return 4 - scalar_diff
 
-			
-#ns = NeuroSystem(3, 1);
-#print "Just created:"
-#print "\n"
-#print(ns);		
+def xor_fitness_debug(neuro_system):
+	inputs =[[0, 0], [0, 1], [1, 0], [1, 1]]
+	expected = [0, 1, 1, 0]
+	actual = [neuro_system.step(i) for i in inputs]
 
-#result = ns.step([1, 2]);
+	print "actual: " + str(actual)
 
-#print "\nAfter step:"
-#print(ns);	
+	diff = [(p - q[0])*(p - q[0]) for p, q in zip(expected, actual)]
 
-#print "\nResult:"
-#print [str(x) for x in result]
+	scalar_diff = sum(diff)
 
+	return 4 - scalar_diff
 
-#ns_partner = NeuroSystem(3, 1)
-#print "ns_partner " + str(ns_partner)
-
-
-#child = ns.evolve(ns_partner)
-#print "child " + str(child)
-
+for i in range(10):
+	print "Run no. ", i
+	ga = GeneticAlgorithm(neuro_system_impl, xor_fitness)
+	ga.run(GENERATIONS, POPULATION)
