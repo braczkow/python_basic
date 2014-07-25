e755111f5d6c469774d0d5baee41feb4a46eb1c7
braczkow <braczkow@gmail.com>
2014-07-25 22:45:00 +0200

    MLP evolution completed. TODO: combie network and evolutor

--- MLP.py
+++ MLP.py
@@ -1,3 +1,7 @@
+import random
+import math
+
+
 class MLPLayer:
 	def __init__(self, n_neurons, n_inputs):
 		self.n_neurons = n_neurons
@@ -7,6 +11,7 @@ class MLPLayer:
 
 		for i in range(self.n_neurons):
 			self._weights.append([0] * (1 + self.n_inputs))
+
 
 	def step(self, inputs):
 		print inputs
@@ -22,6 +27,9 @@ class MLPNetwork:
 	def __init__(self, n_layers, n_neurons_per_layer):
 		self._layers = []
 		self._input_size = n_neurons_per_layer[0]
+
+		self.n_layers = n_layers
+		self.n_neurons_per_layer = n_neurons_per_layer
 
 		for i in range(1, n_layers):
 			self._layers.append(MLPLayer(n_neurons_per_layer[i], n_neurons_per_layer[i-1]))
@@ -46,14 +54,67 @@ class MLPNetworkEvolutor:
 
 	
 	def reproduce(self):
-		_cross()
-		_mutate()
-	
+		self._cross()
+		self._mutate()
+		
 	def _cross(self):
+		if len(self._lhs._layers) != len(self._rhs._layers):
+			print "MLPNetworkEvolutor._cross we are not of same size"
+			return
 
+		self._offspring = MLPNetwork(self._lhs.n_layers, self._lhs.n_neurons_per_layer)
+		
+		offspring_layers = []
+
+		for lhs_layer, rhs_layer in zip(self._lhs._layers, self._rhs._layers):
+			layer = self._cross_layers(lhs_layer, rhs_layer)
+			offspring_layers.append(layer)
+
+		self._offspring._layers = offspring_layers
+
+	def _cross_layers(self, lhs, rhs):
+			layer = MLPLayer(lhs.n_neurons, lhs.n_inputs)
+
+			weights = []
+
+			for lhs_weight, rhs_weight in zip(lhs._weights, rhs._weights):
+				rand = random.uniform(0, 1)
+				if rand > 0.5:
+					weights.append(lhs_weight)
+				else:
+					weights.append(rhs_weight)
+
+			layer._weights = weights
+
+			return layer
+
+			
+
+	def _mutate(self):
+		for l in self._offspring._layers:
+			for n in l._weights:
+				for w in n:
+					w = w + random.normalvariate(0, 0.1)
+
+	def get_offspring(self):
+		return self._offspring
+
+class MLP_NE_System:
+	def __init__(self):
+		self._network = MLPNetwork
 
 
 
 
 nn = MLPNetwork(3, [2, 2, 1])
 nn.step([2, 3])
+
+nn2 = MLPNetwork(3, [2, 2, 1])
+
+mne = MLPNetworkEvolutor(nn, nn2)
+mne.reproduce()
+
+offspring = mne.get_offspring()
+
+
+
