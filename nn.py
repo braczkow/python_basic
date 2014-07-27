91105fe94891ed3e7a0bd18dd55816ce32ebb7b0
braczkow <braczkow@gmail.com>
2014-07-27 12:17:19 +0200

    Revert "MLP dev"
    This reverts commit 73eb3379d4134289e6534df863ab44153a17cc07.

--- nn.py
+++ nn.py
@@ -24,50 +24,10 @@ class WeightsDict:
 
 	def __str__(self):
 		return str(self.weights)
-class MLPLayer:
-	def __init__(self, n_neurons, n_inputs):
-		self.n_neurons = n_neurons
-		self.n_inputs = n_inputs
-		
-		self._weights = []
-
-		for i in range(self.n_neurons):
-			self._weights.append([0] * (1 + self.n_inputs))
-
-	def step(self, inputs):
-		print inputs
-		inputs.append(1)
-		output = []
-		for w in self._weights:
-			inputs_weighted = sum([x*y for x,y in zip(w, inputs)])
-			output.append(1/(1 + math.exp(-inputs_weighted)))
-
-		return output
-
-class MLPNetwork:
-	def __init__(self, n_layers, n_neurons_per_layer):
-		self._layers = []
-		self._input_size = n_neurons_per_layer[0]
-
-		for i in range(1, n_layers):
-			self._layers.append(MLPLayer(n_neurons_per_layer[i], n_neurons_per_layer[i-1]))
-
-	def step(self, inputs):
-		if len(inputs) != self._input_size:
-			print "Input size does not match."	
-			return 
-
-		last_result = inputs
-
-		for l in self._layers:
-			last_result = l.step(last_result)
-
-		return last_result
-
 
 
 class NeuroSystem:
-	def __init__(self, N, n_to_returnzz):
+	def __init__(self, N, n_to_return):
 		self.N = N
 		self.n_to_return = n_to_return
 		self.beta = 5
@@ -234,6 +194,3 @@ class NeuroSystem:
 		offspring._mutate()
 
 		return offspring
-	
-
-
